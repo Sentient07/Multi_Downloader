75e5eb954fe29a5ab39b5451260b7b369b346b5a
sentient07 <vxrram95@gmail.com>
2014-02-17 22:50:30 +0530

    First Commit

---
+++ add_ip.sh
@@ -0,0 +1,12 @@
+ifconfig eth0:1 10.3.12.230 netmask 255.255.255.0
+ifconfig eth0:2 10.3.12.231 netmask 255.255.255.0
+ifconfig eth0:3 10.3.12.232 netmask 255.255.255.0
+ifconfig eth0:4 10.3.12.233 netmask 255.255.255.0
+ifconfig eth0:5 10.3.12.234 netmask 255.255.255.0
+ifconfig eth0:6 10.3.12.235 netmask 255.255.255.0
+ifconfig eth0:7 10.3.12.236 netmask 255.255.255.0
+ifconfig eth0:8 10.3.12.237 netmask 255.255.255.0
+ifconfig eth0:9 10.3.12.238 netmask 255.255.255.0
+ifconfig eth0:10 10.3.12.239 netmask 255.255.255.0
+ifconfig eth0:11 10.3.12.240 netmask 255.255.255.0
+ifconfig eth0:12 10.3.12.241 netmask 255.255.255.0
